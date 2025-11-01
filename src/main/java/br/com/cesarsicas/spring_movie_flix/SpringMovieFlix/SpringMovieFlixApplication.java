84af59baba426ba8502adb5ddba5da399f4e0d65
cesarsicas <jctsiqueira@gmail.com>
2025-11-01 10:22:34 -0300

    first - configured basic libraries

---
+++ src/main/java/br/com/cesarsicas/spring_movie_flix/SpringMovieFlix/SpringMovieFlixApplication.java
@@ -0,0 +1,13 @@
+package br.com.cesarsicas.spring_movie_flix.SpringMovieFlix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMovieFlixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMovieFlixApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/cesarsicas/spring_movie_flix/SpringMovieFlix/SpringMovieFlixApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.cesarsicas.spring_movie_flix.SpringMovieFlix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMovieFlixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
