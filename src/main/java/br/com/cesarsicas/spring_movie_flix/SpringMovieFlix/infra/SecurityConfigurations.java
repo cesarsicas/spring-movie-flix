0dc40d7a231f226a347e2f501bb958e207dfb4c8
cesarsicas <jctsiqueira@gmail.com>
2026-03-09 19:42:04 -0300

    [feat] make GET /transmissions/current public

--- src/main/java/br/com/cesarsicas/spring_movie_flix/SpringMovieFlix/infra/SecurityConfigurations.java
+++ src/main/java/br/com/cesarsicas/spring_movie_flix/SpringMovieFlix/infra/SecurityConfigurations.java
@@ -52,6 +52,7 @@ public class SecurityConfigurations {
                                 .requestMatchers(HttpMethod.POST, "/reviews/**").hasAnyAuthority(RolePermissions.DEFAULT_CREATE.name())
                                 .requestMatchers(HttpMethod.GET, "/reviews/**").hasAnyAuthority(RolePermissions.DEFAULT_CREATE.name())
 
+                                .requestMatchers(HttpMethod.GET, "/transmissions/current").permitAll()
                                 .requestMatchers("/transmissions/**").hasAnyRole(Role.ADMIN.name())
                                 .requestMatchers(HttpMethod.GET, "/transmissions/**").hasAnyAuthority(RolePermissions.ADMIN_READ.name())
                                 .requestMatchers(HttpMethod.POST, "/transmissions/**").hasAnyAuthority(RolePermissions.ADMIN_CREATE.name())
