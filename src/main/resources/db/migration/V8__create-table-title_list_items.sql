4603e8625ffd5344990b05f0263016a97b9fbed0
cesarsicas <jctsiqueira@gmail.com>
2026-05-01 20:23:56 -0300

    [fix] fix the migration file

--- src/main/resources/db/migration/V8__create-table-title_list_items.sql
+++ src/main/resources/db/migration/V8__create-table-title_list_items.sql
@@ -1,7 +1,7 @@
 CREATE TABLE title_list_items (
     id          BIGSERIAL PRIMARY KEY,
     external_id BIGINT NOT NULL UNIQUE,
-    name        VARCHAR(500),
+    title       VARCHAR(500),
     type        VARCHAR(50),
     year        INTEGER,
     imdb_id     VARCHAR(50),
